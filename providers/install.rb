365306cfa4b2478a910a9caae5f4baed0d451f2c
adinata <mail.dieend@gmail.com>
2015-08-06 13:10:17 +0700

    use correct namespace

--- providers/install.rb
+++ providers/install.rb
@@ -69,7 +69,7 @@ action :create do
           tar xzvf #{Chef::Config[:file_cache_path]}/kibana_#{kb_args[:file_version]}_#{kb_args[:name]}.tar.gz -C #{kb_args[:install_dir]}
           chown #{kb_args[:user]} -R #{kb_args[:install_dir]}
         }
-        not_if { File.exists?("#{kb_args[:install_dir]}/kibana_#{kb_args[:file_version]}") }
+        not_if { ::File.directory?("#{kb_args[:install_dir]}/kibana_#{kb_args[:file_version]}") }
       end
 
       new_resource.updated_by_last_action(res.updated_by_last_action?)
